8fc27360108c9618b86a45242fa2c6690e4bc1df
Lukas2380 <lukas.schachinger2380@gmail.com>
2022-11-17 10:45:46 +0100

    Combined the IgnoreFile program with the existing main program.

--- ClassFiles/Program.cs
+++ ClassFiles/Program.cs
@@ -13,15 +13,19 @@ namespace ClassFiles
     {
         static void Main(string[] args)
         {
-            string path = @"D:\_TEMP_\";
-            int linecount = 1;
+            string path = @"C:\Users\lukas\OneDrive - HTBLuVA Mödling\Schule\5AAIFT\POS\Duplikatfinder";
             List<string> fileList = new List<string>();
             List<FilesRead> files = new List<FilesRead>();
             List<Text> lines = new List<Text>();
+            _ = new List<string>();
+
+            string ignorefile = IgnoreFileCreate();
+
             try
             {
-                fileList.AddRange(Directory.GetFiles(path, "*.txt*", SearchOption.AllDirectories));
-            }catch(Exception ex)
+                fileList.AddRange(Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories));
+            }
+            catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
@@ -29,29 +33,106 @@ namespace ClassFiles
 
             for (int i = 0; i < fileList.Count; i++)
             {
+                Console.WriteLine();
+                Console.WriteLine("File: " + fileList[i]);
+                Console.WriteLine();
+
                 lines.Clear();
-                using(StreamReader sr = new StreamReader(fileList[i]))
+
+                List<string> text = Lukas(fileList[i], ignorefile, out List<int> linecount);
+
+                for (int j = 0; j < text.Count(); j++)
                 {
-                    while (!sr.EndOfStream)
+                    try
                     {
-                        try
-                        {
-                            lines.Add(new Text(sr.ReadLine(), linecount));
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.Message);
-                        }
-                        linecount++;
+                        lines.Add(new Text(text[j], linecount[j]));
+                        Console.WriteLine("Line " + linecount[j] + ": " + text[j]);
                     }
-                    
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
                 }
-                linecount = 1;
+
                 FileInfo fileInfo = new FileInfo(fileList[i]);
                 files.Add(new FilesRead(fileInfo.Name, fileInfo.FullName, fileInfo.Extension, lines.ToList()));
             }
-            
             Console.ReadLine();
+        }
+
+        static string IgnoreFileCreate()
+        {
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string ignorefile = Path.Combine(sCurrentDirectory, @"..\..\..\ignore.txt");
+            string ignorefilePath = Path.GetFullPath(ignorefile);
+
+            if (!File.Exists(ignorefile))
+            {
+                using (StreamWriter sw = File.CreateText(ignorefilePath))
+                {
+                    sw.WriteLine("// * = removes line if this symbol is the only thing in the line");
+                    sw.WriteLine("*{");
+                    sw.WriteLine("*}");
+
+                    sw.WriteLine("// removes the line if the line contains this");
+                    sw.WriteLine("using system");
+                    sw.WriteLine("using file");
+                }
+            }
+            return ignorefile;
+        }
+
+        static List<string> Lukas(string csFile, string ignorefile, out List<int> linecount)
+        {
+            List<string> lines = new List<string>();
+            List<string> ignorelines = new List<string>();
+            ignorelines.AddRange(File.ReadAllLines(ignorefile));
+            ignorelines.Remove("");
+            int count = 0;
+            linecount = new List<int>();
+
+            string codeline;
+            bool goodline = true;
+            using (StreamReader reader = new StreamReader(csFile))
+            {
+                while (!reader.EndOfStream)
+                {
+                    count++;
+                    codeline = reader.ReadLine().TrimStart().TrimEnd().ToLower();
+
+                    for (int i = 0; i < ignorelines.Count(); i++)
+                    {
+                        ignorelines[i] = ignorelines[i].ToLower();
+
+                        if (ignorelines[i].StartsWith("*"))
+                        {
+                            if (codeline == ignorelines[i].TrimStart().TrimEnd().Remove(0, 1))
+                            {
+                                goodline = false;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if (codeline.Contains(ignorelines[i]))
+                            {
+                                goodline = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (goodline)
+                    {
+                        if (codeline.Length != 0)
+                        {
+                            lines.Add(codeline);
+                            linecount.Add(count);
+                        }
+                    }
+                    goodline = true;
+                }
+            }
+            return lines;
         }
     }
 }
